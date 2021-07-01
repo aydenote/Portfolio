715d92b536fe3b7e953b0aa506bfcfb85ea3a9bc
WebDevelop2 <dslgpgh@gmail.com>
2021-07-01 23:33:18 +0900

    CSS, JS : Arrow Up button
    아래로 스크롤시 오른쪽 하단에 화살표 모양이 활성화되며, 해당 버튼 클릭시 "#home" 으로 이동.

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.addEventListener('scroll', ()=>{
     } else {
         navbar.classList.remove('navbar__dark');
     }
-})
+});
 
 // Navbar 메뉴 클릭시 스크롤 이동
 const navbarMenu= document.querySelector('.navbar__menu');
@@ -21,25 +21,24 @@ navbarMenu.addEventListener('click', (event)=>{
         return
     } 
     scrollIntoView(link);
-})
+});
 
 // home__ contact me! button 클릭시 스크롤 이동
 const contactMe= document.querySelector('.home__contact');
 contactMe.addEventListener('click', (event)=>{
     const link = event.target.dataset.link;
-    if(link== null){
+     if(link== null){
         return
     } 
     
     scrollIntoView(link);
-})
+});
 
-// 스크롤 이동 코드
+// 스크롤 이동 
 function scrollIntoView(selector){
     const scrollTo= document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
-
-}
+};
 
 // 스크롤시 HOME content 투명화
 
@@ -47,4 +46,22 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', ()=>{
   home.style.opacity= 1 - window.scrollY / homeHeight;
-})
+});
+
+// 이래로 스크롤시 arrow top button 활성화
+const arrowUp = document.querySelector('.arrow-up')
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY > homeHeight /2 ){
+        arrowUp.classList.add('visible');
+    } else{
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// arrow top button 클릭시 페이지 맨 위로 이동.
+arrowUp.addEventListener('click', ()=>{
+    scrollIntoView('#home');
+});
+
+
+
