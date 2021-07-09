75ae9c41ccf4c10d769ad49acf77b33d323876f4
WebDevelop2 <dslgpgh@gmail.com>
2021-07-09 11:55:06 +0900

    JS : Project filtering

--- main.js
+++ main.js
@@ -64,4 +64,29 @@ arrowUp.addEventListener('click', ()=>{
 });
 
 
+// projects 클릭시 필터링 및 애니메이션 추가
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
 
+workBtnContainer.addEventListener('click', (event)=>{
+    const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
+
+    if(filter == null){
+        return;
+    }
+
+    projectContainer.classList.add('anim-out');
+    
+
+    setTimeout(()=>{
+        projects.forEach((project)=>{
+            if(filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            } else{
+                project.classList.add('invisible');
+            }
+        })
+        projectContainer.classList.remove('anim-out');},300);
+    
+})
